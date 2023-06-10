ced46cab908bee253b62a543fe73ac387876f247
David King <ddk225@lehigh.edu>
2023-06-10 19:01:39 -0400

    commit to main

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn djangoproj:application
+web: gunicorn djangoproj.wsgi --log-file -
--- djangoproj/movie_site/settings.py
+++ djangoproj/movie_site/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECRET_KEY = 'odoPq7S7IF59G2sW3SxOJuVsYtTn6mTD'
